9b366163e014b914b39a21dfa822fb267a7bd401
aagottes <aagottesman@gmail.com>
2023-02-07 14:24:11 +0200

    macros and tests were added

--- target/run/dbtlearn2/models/fct/fct_reviews.sql
+++ target/run/dbtlearn2/models/fct/fct_reviews.sql
@@ -1,9 +1,26 @@
-begin;
+
+  
     
 
-        insert into airbnb.dev.fct_reviews ("LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT")
+        create or replace transient table airbnb.dev.fct_reviews  as
         (
-            select "LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT"
-            from airbnb.dev.fct_reviews__dbt_tmp
+WITH  __dbt__cte__src_reviews as (
+with src_reviews as (
+select LISTING_ID, DATE as review_date, REVIEWER_NAME, COMMENTS as review_text,
+SENTIMENT as review_sentiment from airbnb.raw.raw_reviews)
+select * from src_reviews
+),src_reviews AS (
+ SELECT * FROM __dbt__cte__src_reviews
+)
+SELECT 
+
+    
+md5(cast(coalesce(cast(listing_id as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(review_date as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(reviewer_name as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(review_text as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) as review_id,
+*
+
+ FROM src_reviews
+WHERE review_text is not null
+
         );
-    commit;
+      
+  
